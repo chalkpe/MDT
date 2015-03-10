9221a015c252181c5322d6f239ddcc3c9436dc1f
ChalkPE <amato0617@gmail.com>
2015-03-11 00:20:17 +0900

    Added error images

--- app/src/main/java/com/mcpekorea/mdt/ProjectExporter.java
+++ app/src/main/java/com/mcpekorea/mdt/ProjectExporter.java
@@ -4,80 +4,80 @@ import java.io.IOException;
 import java.util.List;
 
 /**
- * @since 2015-03-07
- * @author onebone <jyc0410@naver.com>
- */
+* @since 2015-03-07
+* @author onebone <jyc0410@naver.com>
+*/
 public class ProjectExporter {
-	private Project project;
+    private Project project;
 
-	public static final int HEADER_END = 6;
+    public static final int HEADER_END = 6;
 
-	public ProjectExporter(Project project){
-		this.project = project;
-	}
+    public ProjectExporter(Project project){
+        this.project = project;
+    }
 
-	public byte[] create() throws IOException{
-		int length = 0;
+    public byte[] create() throws IOException {
+        int length = 0;
 
-		String author = project.getAuthor();
-		int offsetShift = author.length();
+        String author = project.getAuthor();
+        int offsetShift = author.length();
 
-		List<Patch> patches = project.getPatches();
-		length += (HEADER_END + offsetShift + (4 * patches.size()));
-		for(Patch patch : patches){
-			byte[] valueBin = patch.getValue().getBytes();
-			length += (valueBin.length + 4);
-		}
-		byte[] mod = new byte[length];
-		mod = writeBytes(mod, new byte[]{(byte) 0xff, 0x50, 0x54, 0x50, 0x00}, 0);
+        List<Patch> patches = project.getPatches();
+        length += (HEADER_END + offsetShift + (4 * patches.size()));
+        for(Patch patch : patches){
+            byte[] valueBin = patch.getValue().getBytes();
+            length += (valueBin.length + 4);
+        }
+        byte[] mod = new byte[length];
+        mod = writeBytes(mod, new byte[]{(byte) 0xff, 0x50, 0x54, 0x50, 0x00}, 0);
 
-		mod[5] = (byte) patches.size();
-		int patchStart = HEADER_END + offsetShift + (4 * patches.size());
-		int header = HEADER_END + offsetShift;
+        mod[5] = (byte) patches.size();
+        int patchStart = HEADER_END + offsetShift + (4 * patches.size());
+        int header = HEADER_END + offsetShift;
 
-		byte[] authorBin = author.getBytes();
+        byte[] authorBin = author.getBytes();
 
-		mod = writeBytes(mod, authorBin, HEADER_END + 1);
+        mod = writeBytes(mod, authorBin, HEADER_END + 1);
 
-		for(Patch patch : patches){
-			Offset offset = patch.getOffset();
-			Value value = patch.getValue();
+        for(Patch patch : patches){
+            Offset offset = patch.getOffset();
+            Value value = patch.getValue();
 
-			byte[] offsetBin = offset.getBytes();
-			byte[] locBin = intToByteArray(header);
-			mod = writeBytes(mod, locBin, header);
-			mod = writeBytes(mod, offsetBin, patchStart);
+            byte[] offsetBin = offset.getBytes();
+            byte[] locBin = intToByteArray(header);
+            mod = writeBytes(mod, locBin, header);
+            mod = writeBytes(mod, offsetBin, patchStart);
 
-			byte[] valueBin = value.getBytes();
-			mod = writeBytes(mod, valueBin, patchStart + 4);
+            byte[] valueBin = value.getBytes();
+            mod = writeBytes(mod, valueBin, patchStart + 4);
 
-			patchStart += (4 + valueBin.length);
-			header += 4;
-		}
+            patchStart += (4 + valueBin.length);
+            header += 4;
+        }
 
-		return mod;
-	}
+        return mod;
+    }
 
-	public static final byte[] writeBytes(byte[] src, byte[] c, int start){
-		for(int i = start; i < c.length + start; i++){
-			src[i] = c[i - start];
-		}
-		return src;
-	}
+    public static final byte[] writeBytes(byte[] src, byte[] c, int start){
+        for(int i = start; i < c.length + start; i++){
+            src[i] = c[i - start];
+        }
+        return src;
+    }
 
-	public static final byte[] intToByteArray(int value) {
-		return new byte[] {
-				(byte)(value >>> 24),
-				(byte)(value >>> 16),
-				(byte)(value >>> 8),
-				(byte)value
-		};
-}
+    public static final byte[] intToByteArray(int value) {
+        return new byte[] {
+        (byte)(value >>> 24),
+        (byte)(value >>> 16),
+        (byte)(value >>> 8),
+        (byte)value
+        };
+    }
 
-public static final int byteArrayToInt(byte [] b) {
-	return (b[0] << 24)
-				+ ((b[1] & 0xFF) << 16)
-				+ ((b[2] & 0xFF) << 8)
-				+ (b[3] & 0xFF);
-	}
+    public static final int byteArrayToInt(byte [] b) {
+        return (b[0] << 24)
+        + ((b[1] & 0xFF) << 16)
+        + ((b[2] & 0xFF) << 8)
+        + (b[3] & 0xFF);
+    }
 }
