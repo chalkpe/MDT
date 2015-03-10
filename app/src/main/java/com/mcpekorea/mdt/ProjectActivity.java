5d98ff7e54c36720e31a1bc0dea0f09e6f0735f9
onebone <yoonchul.jeong00@gmail.com>
2015-03-10 20:53:05 +0900

    Removed trimming from changing author name

--- app/src/main/java/com/mcpekorea/mdt/ProjectActivity.java
+++ app/src/main/java/com/mcpekorea/mdt/ProjectActivity.java
@@ -119,7 +119,7 @@ public class ProjectActivity extends ActionBarActivity {
                     .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-                            project.setAuthor(authorArea.getText().toString().trim());
+                            project.setAuthor(authorArea.getText().toString());
                         }
                     })
                     .setNegativeButton(android.R.string.cancel, null)
